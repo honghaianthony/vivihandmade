70f817f88897f394ca56a3963b08bea1a0d7a59f
Duy Nguyen <nguyenngockhuongduy172@gmail.com>
2022-04-07 14:00:34 +0700

    add axios

---
+++ axios/axiosInstance.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5000/api/v1",
+});
+
+// axiosInstance.defaults.headers.common[
+//     "Authorization"
+// ] = `Bearer ${localStorage.getItem("token")}`;
+
+// axiosInstance.interceptors.request.use(function (config) {
+//     return config;
+//   }, function (error) {
+//     return Promise.reject(error);
+//   });
+
+axiosInstance.interceptors.response.use(
+    function (response) {
+        return response.data;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
